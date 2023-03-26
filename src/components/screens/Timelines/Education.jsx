d19298cae7c7c82272e268898f8f0afd270c8cd4
Raj Yadav <84654548+ItsRaJYadav@users.noreply.github.com>
2023-03-26 23:15:57 +0530

    Update Education.jsx

--- src/components/screens/Timelines/Education.jsx
+++ src/components/screens/Timelines/Education.jsx
@@ -3,20 +3,17 @@ import React from 'react'
 const Education = () => {
     return (
         <>
-
-
             <ol className="relative border-l border-gray-200 dark:border-gray-700">
                 <li className="mb-10 ml-4">
                     <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
                     <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                        June 2019-May 2023
+                    June 2019-May 2023
                     </time>
-                    <h3 className="text-lg font-semibold  dark:text-white">
-
-                        J.C. Bose University of Science and Technology, YMCA
+                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                    J.C. Bose University of Science and Technology, YMCA
                     </h3>
                     <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-                        B.tech degree in the Specialization in electronics and computer Enginering
+                    B.tech degree in the Specialization in electronics and computer Enginering
 
                     </p>
 
@@ -24,46 +21,46 @@ const Education = () => {
                 <li className="mb-10 ml-4">
                     <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
                     <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                        June 2016-May 2018
+                    June 2016-May 2018
                     </time>
-                    <h3 className="text-lg font-semibold  dark:text-white">
-
-                        Progressive Central High School
+                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                    Progressive Central High School
                     </h3>
                     <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-                        I have completed the secondary school from here which is equivalent to the 10+2 degree course means that intermediate after matriculations
+                    I have completed the secondary school from here which is equivalent to the 10+2 degree course means that intermediate after matriculations
                     </p>
                 </li>
                 <li className="mb-10 ml-4">
                     <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
                     <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                        April 2014-
+                    April 2014-
                         May 2016
                     </time>
-                    <h3 className="text-lg font-semibold  dark:text-white">
-
-                        Woodbine Modern School
+                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                    Woodbine Modern School
                     </h3>
                     <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-                        Secondary Education
+                    Secondary Education
                     </p>
                 </li>
                 <li className="ml-4">
                     <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
                     <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                        February 2006-March 2014
+                    February 2006-March 2014
                     </time>
-                    <h3 className="text-lg font-semibold  dark:text-white">
-
-                        Biraul Central School
+                    <h3 className="text-lg font-semibold text-white">
+                    Biraul Central School
                     </h3>
                     <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-                        Primary Education
+                    Primary Education
                     </p>
                 </li>
             </ol>
         </>
     )
 }
+
+//
+
 
 export default Education
