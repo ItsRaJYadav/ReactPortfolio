a21973d8df48dc06aad0ffec889a49502a93b273
Raj Yadav <84654548+ItsRaJYadav@users.noreply.github.com>
2023-03-26 23:52:25 +0530

    Update Nav.jsx

--- src/components/global/Nav.jsx
+++ src/components/global/Nav.jsx
@@ -50,7 +50,7 @@ const NavBar = () => {
 
             <button
               type="button"
-              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-white dark:hover:bg-gray-800 dark:focus:ring-gray-600"
               aria-controls="navbar-cta"
               aria-expanded={isMenuOpen}
               onClick={toggleMenu}
